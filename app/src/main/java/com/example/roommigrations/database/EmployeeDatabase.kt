8d2cd8a01921e8f53483d16f28a9a4e220f04cde
Karthiga <lkarthigalm@gmail.com>
2023-05-16 19:56:37 +0530

    Migration to Version 3

--- app/src/main/java/com/example/roommigrations/database/EmployeeDatabase.kt
+++ app/src/main/java/com/example/roommigrations/database/EmployeeDatabase.kt
@@ -7,7 +7,7 @@ import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.roommigrations.models.Employee
 
 
-@Database(entities = [Employee::class] , version = 2, exportSchema = false)
+@Database(entities = [Employee::class] , version = 3, exportSchema = false)
 abstract class EmployeeDatabase : RoomDatabase() {
 
     abstract val dao: EmployeeDao
@@ -22,6 +22,31 @@ abstract class EmployeeDatabase : RoomDatabase() {
                 database.execSQL("DROP TABLE employee")
                 database.execSQL("ALTER TABLE employeeNew RENAME TO employee")
             }
+        }
+
+            val migration_2_3 = object : Migration(2, 3) {
+                override fun migrate(database: SupportSQLiteDatabase) {
+
+                    database.execSQL("CREATE TABLE employeeNew (name TEXT NOT NULL, designation TEXT NOT NULL,id INTEGER NOT NULL, gender TEXT NOT NULL, salary INTEGER NOT NULL,  age INTEGER NOT NULL, PRIMARY KEY(id))")
+                    database.execSQL("INSERT INTO employeeNew (name,designation,id, gender, salary, age) Select name ,designation, id , gender, salary, case when  age REGEXP \'^[0-9]+\$\' then  age else 0 end as age  from employee")
+                    database.execSQL("DROP TABLE employee")
+                    database.execSQL("ALTER TABLE employeeNew RENAME TO employee")
+
+                }
+
+            }
+
+                val migration_1_3 = object : Migration(1, 3) {
+                    override fun migrate(database: SupportSQLiteDatabase) {
+
+                        database.execSQL("CREATE TABLE employeeNew (name TEXT NOT NULL, designation TEXT NOT NULL,id INTEGER NOT NULL, gender TEXT NOT NULL, salary INTEGER NOT NULL,  age INTEGER NOT NULL, PRIMARY KEY(id))")
+                        database.execSQL("INSERT INTO employeeNew (name,designation,id, gender, salary, age) Select name ,designation, id , case when gender = \"FEMALE\" then \"F\" when gender = \"MALE\" then \"M\"  else \"O\" end as gender, salary, case when  age REGEXP \'^[0-9]+\$\' then  age else 0 end as age  from employee")
+                        database.execSQL("DROP TABLE employee")
+                        database.execSQL("ALTER TABLE employeeNew RENAME TO employee")
+
+                    }
+
+                }
 
         }
 
@@ -39,7 +64,7 @@ abstract class EmployeeDatabase : RoomDatabase() {
                         context.applicationContext,
                         EmployeeDatabase::class.java,
                         "EmployeeDetails"
-                    ).allowMainThreadQueries().addMigrations(migration_1_2)
+                    ).allowMainThreadQueries().addMigrations(migration_1_2, migration_2_3, migration_1_3)
                         .build()
 
                     INSTANCE = instance
--- app/src/main/java/com/example/roommigrations/models/Employee.kt
+++ app/src/main/java/com/example/roommigrations/models/Employee.kt
@@ -22,14 +22,30 @@ import kotlinx.android.parcel.Parcelize
 
 
 // Version 2
+//@Entity(tableName = "employee")
+//@Parcelize
+//data class Employee(
+//    val name:String,
+//    val age:String,
+//    val gender:String,
+//    val salary: Int,
+//    val designation:String,
+//    @PrimaryKey
+//    val id:Long = System.currentTimeMillis()
+//) :Parcelable
+//
+
+//Version 3
 @Entity(tableName = "employee")
 @Parcelize
 data class Employee(
     val name:String,
-    val age:String,
+    val age:Int,
     val gender:String,
     val salary: Int,
     val designation:String,
     @PrimaryKey
     val id:Long = System.currentTimeMillis()
 ) :Parcelable
+
+
