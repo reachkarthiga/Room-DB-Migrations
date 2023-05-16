a4c0237156273b9d9b61e3ad2bebb26c5b01b463
Karthiga <lkarthigalm@gmail.com>
2023-05-16 19:20:37 +0530

    Bug Fix - Gender Case sensitivity

--- app/src/main/java/com/example/roommigrations/AddEmployee.kt
+++ app/src/main/java/com/example/roommigrations/AddEmployee.kt
@@ -27,7 +27,7 @@ class AddEmployee : AppCompatActivity() {
                         Employee(
                             editText_name.text.toString(),
                             editText_age.text.toString(),
-                            editText_gender.text.toString(),
+                            editText_gender.text.toString().toUpperCase(),
                             editText_salary.text.toString().toInt(),
                             editText_designation.text.toString()
                         )
